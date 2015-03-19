f2830fa96a8b35aaf8d87276fb03a8df88405f48
jhonson1195 <jhonson1195@gmail.com>
2015-03-18 22:28:15 -0600

    Implementacion de lista doblemente enlazada

--- src/dbs_project/storage/impl/DoublyLinkedList.java
+++ src/dbs_project/storage/impl/DoublyLinkedList.java
@@ -11,17 +11,38 @@ import dbs_project.structures.LinearList;
 /**
  *
  * @author jhonson
+ * @param <T>
  */
-public class DoublyLinkedList implements LinearList {
-
-    @Override
-    public void insert(Object element) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+public class DoublyLinkedList <T>implements LinearList<T> {
+    
+    private Node<T> head, current, tail;
+    private int position;
+    private int size;
+    
+    public DoublyLinkedList(){
+        head=new Node<>();
+        current = head;
+        tail = head;
+        size = 0;
+        position = -1;
     }
 
     @Override
-    public void append(Object element) {
+    public void insert(T element) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    
+    }
+
+    @Override
+    public void append(T element) {
+        Node newnode = new Node<T>(element);
+        tail.setNext(newnode);
+        newnode.setLast(tail);
+        tail=newnode;
+        size++;
+        position++;
+     
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates. 
     }
 
     @Override
@@ -30,7 +51,7 @@ public class DoublyLinkedList implements LinearList {
     }
 
     @Override
-    public Object getElement() {
+    public T getElement() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
@@ -89,4 +110,45 @@ public class DoublyLinkedList implements LinearList {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
+    public class Node <T>{
+        private Node<T> next;
+        private Node<T> last;
+        private T Date;
+        
+        public Node(){
+            next=null;
+            last=null;
+            Date=null;
+        }
+        public Node(T Date){
+            next=null;
+            last=null;
+            this.Date=Date;
+        }
+        public Node(T Date, Node<T> next,Node<T> last){
+            this.Date= Date;
+            this.next=next;
+            this.last=last; 
+        }
+        
+        public T getDate(){
+            return Date;
+        }
+        public void setDate(T Date){
+            this.Date= Date;
+        }
+        public Node<T> getNext(){
+            return next;
+        }
+        public void setNext(Node<T> next){
+            this.next=next;
+        }
+        public Node<T> getLast(){
+            return last;
+        }
+        public void setLast(Node<T> last){
+            this.last=last;
+        }
+        
+    }
 }
