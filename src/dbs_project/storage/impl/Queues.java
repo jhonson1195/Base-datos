1b575470d6ca23d9d58133616afe719a0801005a
estebanmorales1594 <estebanmorales1594@hotmail.com>
2015-03-22 08:57:24 -0600

    Queues Simple
    Gente ahi masomenos agregue lo que tengo de Queues....
    - Unas preguntas:
    * hay que implementar una clase Node, para cada estructura??
    * Como se hace el DataStructure
    Ahi cualquier cosa que ocupen me escriben

--- src/dbs_project/storage/impl/Queues.java
+++ src/dbs_project/storage/impl/Queues.java
@@ -8,45 +8,99 @@ package dbs_project.storage.impl;
 import dbs_project.structures.DataStructure;
 import dbs_project.structures.Queue;
 
-/**
- *
- * @author jhonson
- */
-public class Queues implements Queue {
-
-    @Override
-    public void enqueue(Object element) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+public class Queues <T> implements Queue<T> {
+    private Node<T> front;
+    private Node<T> rear;
+    private int size;
+    
+    public Queues(){
+        this.front = new Node<>();
+        this.rear = this.front;
+        this.size = 0;
     }
 
-    @Override
-    public Object dequeue() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void enqueue(T element) {
+        this.rear.setNext(new Node<T>(element, null));
+        this.rear = rear.getNext();
+        this.size++;
     }
 
-    @Override
-    public Object first() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public T dequeue() {
+        if(this.size == 0){
+            System.out.println("Cola esta vacia");
+            return null;
+        }
+        T temp = this.front.getNext().getElement();
+        Node<T> newTemp = this.front.getNext();
+        this.front.setNext(newTemp.getNext());
+        if (this.rear == newTemp){
+            this.rear = this.front;
+        }
+        this.size--;
+        return temp;
     }
 
-    @Override
+    public T first() {
+        if(this.size == 0){
+            System.out.println("Cola esta vacia");
+            return null;
+        }
+        return this.front.getNext().getElement();
+    }
+
     public int size() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return this.size;
     }
 
-    @Override
     public void clear() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.front = new Node<>();
+        this.rear = this.front;
+        this.size = 0;
     }
 
-    @Override
     public DataStructure getType() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @Override
     public boolean isEmpty() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return size==0;
     }
     
+    private class Node<T>{
+        private T element;
+        private Node<T> next;
+
+        // CONSTRUCTORES "Node"
+        public Node(){
+            this.element = null;
+            this.next = null;
+        }
+
+        public Node(T element){
+            this.element = element;
+            this.next = null;
+        }
+
+        public Node(T element, Node<T> next){
+            this.element = element;
+            this.next = next;
+        }
+
+        // METODOS "Node"
+        public T getElement(){
+            return this.element;
+        }
+
+        public void setElement(T element){
+            this.element = element;
+        }
+
+        public Node<T> getNext(){
+            return this.next;
+        }
+
+        public void setNext(Node<T> next){
+            this.next = next;
+        }
+    }
 }
