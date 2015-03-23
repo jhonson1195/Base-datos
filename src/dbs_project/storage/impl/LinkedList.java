2e42a1986ca48501268e40100660b0eee039dc0a
carc1903 <carlos19ramirez@hotmail.com>
2015-03-23 00:29:36 -0600

    lista Simple
    Implementación Lista simplemente enlazada

--- src/dbs_project/storage/impl/LinkedList.java
+++ src/dbs_project/storage/impl/LinkedList.java
@@ -9,84 +9,319 @@ import dbs_project.structures.DataStructure;
 import dbs_project.structures.LinearList;
 
 /**
- *
- * @author jhonson
+ *Lista doblemente enlazada implementada con la interfaz LinearList generica
+ * @author Carlos Ramirez
+ * @param <T>
  */
-public class LinkedList implements LinearList {
+public class LinkedList<T> implements LinearList <T> {
+        
+//Definicion de atributos
+    Node<T> head, current, tail;
+    private int position;
+    private int size;
+    /**
+     * contructor que inicializa las variables
+     */
+    public LinkedList(){
+        head=new Node<>();
+        current = head;
+        tail = head;
+        size = 0;
+        position = -1;
+    }
+    
+    @Override
+    /**
+     * Inserta un elemento en la lista despues del nodo actual.
+     * @param <T> variable generica
+     */
+    
+    public void insert(T element) {
+        Node<T> newNode = new Node<T>(element, this.current.getNext());
+	this.current.setNext(newNode);
+	if (this.current == this.tail) {
+            this.tail = current.getNext();
+            }
+	this.size++;
+    }
+    
+     /**
+     * Agrega un nodo despues de la cola actual 
+     * @param <T> variable generica
+     */
 
     @Override
-    public void insert(Object element) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void append(T element) {
+	Node<T> newNode = new Node<T>(element);
+	this.tail.setNext(newNode);
+	this.tail = newNode;
+	this.size++;        
+        
     }
 
     @Override
-    public void append(Object element) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
+    
+    /**
+     * Elimina el nodo actual nodo
+     */
     public void remove() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if ((this.head == this.current) && (this.head == this.tail)){
+        	System.out.println("Lista vacía, no se puede remover ningún elemento");
+		return;
+	}
+        Node<T> temp = head;
+    	while (temp.getNext() != this.current) {
+            temp = temp.getNext();
+	}
+        temp.setNext(this.current.getNext());
+    	if (this.current == this.tail) {   
+            this.current = this.tail = temp;
+            this.position--;
+	}
+			
+	else
+            this.current = this.current.getNext();
+        this.size--;
+
     }
 
     @Override
-    public Object getElement() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    /**
+     * Devuelve el elemento contenido en el nodo actual
+     * @return <T> element
+     */
+    public T getElement() {
+        return current.getElement(); 
     }
 
     @Override
+    
+    /**
+     * Devuelve true o false para avanzar el nodo de posicion actual
+     * @return boolean
+     */
     public boolean next() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       if(current==tail){
+           System.out.println("no se puede avanzar");
+           return false;
+       }
+       current=current.getNext();
+       position++;
+       return true;
     }
 
+    
     @Override
+     /**
+     * Devuelve true o false para retroceder el nodo de posicion actual
+     * @return boolean
+     */
     public boolean previous() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (this.current == this.head) {
+            System.out.println("Actualmente en primer nodo, no se puede retroceder");
+            return false;
+	}
+	Node<T> temp = head;
+            this.position = -1;
+            while (temp.getNext() != this.current) {
+		temp = temp.getNext();
+		this.position++;
+            }
+	this.current = temp;
+        return true;
     }
 
-    @Override
+
+    @Override 
+    
+     /**
+     * Retorna la posicion actual del nodo
+     * @return int
+     */
+    
     public int getPosition() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return this.position;
     }
 
     @Override
+    
+     /**
+     * El puntero actual se convierte al primer nodo de la lista
+     * Mueve el puntero current al inicio de la lista
+     */
     public void goToStart() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    this.current = this.head;
+    this.position = -1;
     }
 
     @Override
+    /**
+     * El puntero actual se convierte al ultimo nodo de la lista
+     * Mueve el puntero current al final de la lista
+     */
     public void goToEnd() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	this.current = this.tail;
+        this.position = this.size - 1;       
     }
-
+    
     @Override
+    /**
+     * Mueve el nodo actual a la posicion indicada
+     * @param pos
+     */
     public void goToPos(int pos) throws IndexOutOfBoundsException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (pos < 0 || pos >= this.size) {
+            System.out.println("Posición inválida");
+            return;
+        }
+        if (pos > this.position) {
+            while (pos > this.position) {
+                this.next();
+            }
+        } else if (pos < this.position) {
+            while (pos < this.position) {
+                this.previous();
+            }
+        }
     }
 
     @Override
-    public int getPositionOfElement(Object element) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+     /**
+     * Busca el elemento en la lista y devuelve su indice
+     * @param <T>
+     */
+    public int getPositionOfElement(T element) {
+        Node<T> tempNode = this.head;
+	int position = -1;
+	while (tempNode != null) { 
+            if (tempNode.getElement() != null && tempNode.getElement().equals(element)){
+                return position;
+            }
+	tempNode = tempNode.getNext();
+	position++;
+        }
+	return -1;
     }
 
     @Override
+     /**
+     * Devuelve el tipo de lista
+     */
     public DataStructure getType() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @Override
+    @Override  
+     /**
+     * Devuelve el tamaño de la lista
+     * @return size
+     */
     public int size() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return this.size;
     }
 
     @Override
+     /**
+     * Borra todos los nodos de la lista y crea una nueva
+     */
     public void clear() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        head=new Node<>();
+        current = head;
+        tail = head;
+        size = 0;
+        position = -1; 
     }
 
     @Override
+      /**
+     * Comprueba si la lista esta vacia
+     * @return boolean
+     */
     public boolean isEmpty() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(size>0){
+            return false;
+        }
+        else{
+            return true;
+        }
     }
     
+      /**
+     * Clase para crear una variable de tipo Nodo
+     * @param <T>
+     */
+    
+    public String toString() {
+		Node currentNode = this.head.getNext();
+		
+		StringBuffer result = new StringBuffer();
+        
+        for (int i = 0; currentNode != null; i++) 
+		{
+        	if (i > 0) 
+			{
+        		result.append(",");
+        	}
+        	Object element = currentNode.getElement();
+			
+        	result.append(element == null ? "" : element);
+        	currentNode = currentNode.getNext();
+        }
+        return result.toString();
+    }
+    
+    public class Node <T>{
+        private Node<T> next; 
+        private T Element;
+        
+        /**
+         * Contrutor que inicializa las variables
+         */
+        public Node(){
+            next=null;
+            Element=null;
+        }
+         /**
+         * Contrutor que inicializa las variables
+         */
+        public Node(T Element){
+            next=null;
+            this.Element=Element;
+        }
+         /**
+         * Contrutor que inicializa las variables
+         */
+        public Node(T Element, Node<T> next){
+            this.Element= Element;
+            this.next=next; 
+        }
+         /**
+         * Devuelve el elemento del nodo
+         * @return Date
+         */
+        public T getElement(){
+            return Element;
+        }
+         /**
+         * Define la varialbe del nodo
+         * @param Element
+         */
+        public void setElement(T Element){
+            this.Element= Element;
+        }
+         /**
+         * Devuelve el Nodo de la variable next
+         * @return next
+         */
+        public Node<T> getNext(){
+            return next;
+        }
+         /**
+         * Define el noso siguiente
+         * @param next
+         */
+        public void setNext(Node<T> next){
+            this.next=next;
+        }
+    }
 }
--- src/dbs_project/storage/impl/Preubas.java
+++ src/dbs_project/storage/impl/Preubas.java
@@ -15,8 +15,7 @@ import dbs_project.structures.Stack;
 public class Preubas {
     
     public static void main(String[] args) {
-        
-        /*LinearList<Integer> nuevo= new DoublyLinkedList<>();
+                LinearList<Integer> nuevo= new DoublyLinkedList<>();
         nuevo.append(6);
         nuevo.append(3);
         nuevo.append(9);
@@ -39,8 +38,7 @@ public class Preubas {
         nuevo.append(7);
         System.out.println(nuevo.toString());
         nuevo.clear();
-        System.out.println(nuevo.isEmpty());*/
-        
+        System.out.println(nuevo.isEmpty());
         Stack<Integer> pilaEnteros = new Stacks<>();
         pilaEnteros.push(5);
         pilaEnteros.push(3);
@@ -51,6 +49,85 @@ public class Preubas {
         pilaEnteros.top();
         System.out.println(pilaEnteros.toString());
     
-    }
-    
+  
+        
+        
+        	System.out.println("**** LikedList");
+		System.out.println("**** Agregando elementos....");
+		LinkedList<Integer> lista = new LinkedList<>();
+		lista.append(9);
+		lista.append(11);
+		lista.append(13);
+		
+		System.out.println("**** Tamaño: " + lista.size());
+		System.out.println("**** Representación gráfica: " + lista.toString());
+		
+		System.out.println("**** Recorrer la lista");
+		for(lista.goToStart(); lista.getPosition() < lista.size(); lista.next()) {
+			System.out.println("Pos actual:" + lista.getPosition());
+			System.out.println("Valor actual:" + lista.getElement());
+			System.out.println("@");
+			if (lista.getPosition() == lista.size() -1) {
+				break;
+			}
+		}
+		System.out.println("**** Insertar el 12, de forma ordenada y luego al final el 14");
+		System.out.println("**** Pos actual " + lista.getPosition());
+		for(lista.goToStart(); lista.getPosition() < lista.size(); lista.next()) {
+			if ((lista.getElement()) != null) {
+				if (lista.getElement().equals(11)) {
+					lista.insert(12);
+					lista.append(14);
+					break;
+					}
+			}
+		}
+		System.out.println("**** Tamaño: " + lista.size());
+		System.out.println("**** Representación gráfica: " + lista.toString());
+		/*System.out.println("Reverseeeeeeee");
+		lista.reverse();
+		System.out.println("**** Representación gráfica: " + lista.toString());
+		System.out.println("**** Head: " + lista.head.getElement());
+		System.out.println("**** Current: " + lista.current.getElement());
+		System.out.println("**** Tail: " + lista.tail.getElement());
+		System.out.println("**** Pos: " + lista.getPosition());
+		System.out.println("**** Size: " + lista.getSize());*/
+		System.out.println("**** Pos actual luego de insertar: " + lista.getPosition());
+		System.out.println("**** Buscar el elemento 9, la pos es: " + lista.getPositionOfElement(9));
+		System.out.println("**** Buscar el elemento 14, la pos es: " + lista.getPositionOfElement(14));
+		System.out.println("**** Buscar el elemento 15, la pos es: " + lista.getPositionOfElement(15));
+		System.out.println("**** Obtener elemento en la posición actual (1), el elemento es: " + lista.getElement());
+		lista.goToPos(4);
+		System.out.println("**** Ir a la posición (4), el elemento es: " + lista.getElement());
+		System.out.println("**** Borrando elemento actual ... " + lista.getElement());
+		lista.remove();
+		System.out.println("*** El  elemento en el tail es: " + lista.tail.getElement());
+		System.out.println("*** La posición actual es " + lista.getPosition());
+		System.out.println("**** Tamaño: " + lista.size());
+		System.out.println("**** Representación gráfica: " + lista.toString());
+		lista.previous();
+		System.out.println("**** Retroceder una posición, el elemento actual es: " + lista.getElement());
+		System.out.println("*** La posición actual es " + lista.getPosition());
+		System.out.println("**** Borrando elemento actual ... " + lista.getElement());
+		lista.remove();
+		System.out.println("**** Tamaño: " + lista.size());
+		System.out.println("**** Representación gráfica: " + lista.toString());
+		System.out.println("*** La posición actual es " + lista.getPosition());
+		System.out.println("**** El elemento actual es ... " + lista.getElement());
+		System.out.println("**** Borrando elemento actual ... " + lista.getElement());
+		lista.remove();
+		System.out.println("**** Tamaño: " + lista.size());
+		System.out.println("**** Representación gráfica: " + lista.toString());
+		System.out.println("**** Head: " + lista.head.getElement());
+		System.out.println("**** Current: " + lista.current.getElement());
+		System.out.println("**** Tail: " + lista.tail.getElement());
+		System.out.println("**** Pos: " + lista.getPosition());
+		System.out.println("**** Size: " + lista.size());
+		System.out.println("**** Limpiando lista... ");
+		lista.clear();
+		System.out.println("**** Tamaño: " + lista.size());
+		System.out.println("**** Representación gráfica: " + lista.toString());
+	}
+  
 }
+
